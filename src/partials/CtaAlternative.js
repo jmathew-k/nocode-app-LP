48aad2e3732081ce0c40c49a01c5897d5614c734
Joshua Mathew <JoshuaKMathew@gmail.com>
2022-05-16 17:27:46 +0530

    test

--- src/partials/CtaAlternative.js
+++ src/partials/CtaAlternative.js
@@ -23,9 +23,10 @@ function CtaAlternative() {
 							<div>
 								<a
 									href='https://us13.list-manage.com/contact-form?u=de3b7468d46b98fc48453220a&form_id=ffc6eabbe23cfb0762a4808b4417de2d'
-									button class =' text-black text-sm bg-white hover:bg-gray-100'
+									button
+									class=' hidden md:block px-3 py-2 text-sm rounded text-gray-200 bg-gray-900 hover:bg-gray-800 ml-3s'
 								>
-									Get In Touch
+									<span>Contact</span>
 								</a>
 							</div>
 						</div>
